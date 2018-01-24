0ec5e88ea651b49fed4b57d5a2ba3b56685bb30a
Nayan <nayan.v@crestinfosystems.net>
2018-01-24 16:29:59 +0530

    commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Source: <br />
+https://www.npmjs.com/package/cordova-plugin-headsetdetection <br />
+
+$ ionic cordova plugin add cordova-plugin-headsetdetection <br />
+
+Using non-native/cordova plugin in ionic.<br />
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,37 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    this.audioInit();
   }
 
+  audioJackStatus(){
+    console.log('checking headphone');
+    (<any>window).HeadsetDetection.detect(function(detected){
+      if(detected){
+        alert('Headphone connected')
+      }
+      else{
+        alert('No Headphone Found.')
+      }
+    })
+  }
+
+  audioInit(){
+    try {
+        document.addEventListener('deviceready', function() {
+            (<any>window).HeadsetDetection.registerRemoteEvents(function(status) {
+                switch (status) {
+                  case 'headsetAdded':
+                    alert('Headset connected');
+                  break;
+                  case 'headsetRemoved':
+                    alert('Headset removed');
+                  break;
+                };
+            });
+        }, false);
+      } catch (error) {
+        alert(error);
+      }    
+  }
 }
